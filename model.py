b3120cf8859b6d37a8c1c8d8889334be7d1fe003
Yosif Soliman <yosifsoli981@gmail.com>
2024-04-17 23:32:57 +0200

    added initial code

---
+++ model.py
@@ -0,0 +1,16 @@
+def main():
+    # pre-processing
+
+    # feature selection
+
+    # data splitting
+
+    # model training
+
+    # model testing
+    pass
+
+
+if __name__ == "__main__":
+    main()
+
